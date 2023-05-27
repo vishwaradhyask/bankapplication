f439546e842bf30d708024ed52a2b50b18b5562c
Vishwaradhya Sk <vishwaradhya@qnap.com>
2023-05-27 19:00:24 +0530

    new changes

--- BankBackEnd/BankBackEnd/settings.py
+++ BankBackEnd/BankBackEnd/settings.py
@@ -164,7 +164,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ]
 }
 # REST_FRAMEWORK = {
